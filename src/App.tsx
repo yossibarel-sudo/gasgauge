21e8ee19505cee07190038d14adab6aee2ccb82d
Yossi BarEl <yossi.barel@gmail.com>
2026-07-14 23:44:17 +0300

    add application routing and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
 import EquipmentPage from "./pages/EquipmentPage";
 
 export default function App() {
-  return <EquipmentPage />;
+  return (
+    <BrowserRouter>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1 }}
+          >
+            GasGauge
+          </Typography>
+
+          <Button
+            color="inherit"
+            component={Link}
+            to="/"
+          >
+            Dashboard
+          </Button>
+
+          <Button
+            color="inherit"
+            component={Link}
+            to="/equipment"
+          >
+            Equipment
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{ p: 3 }}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/equipment" element={<EquipmentPage />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
